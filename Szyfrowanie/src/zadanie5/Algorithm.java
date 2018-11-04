84737b3f8f9bf8a92946fd27d96c52b167c6ea89
Przemysław Bielecki <33664570+bielprze@users.noreply.github.com>
2018-11-04 17:44:31 +0100

    Add files via upload

---
+++ Szyfrowanie/src/zadanie5/Algorithm.java
@@ -0,0 +1,6 @@
+package zadanie5;
+
+interface Algorithm {
+    String crypt(String s);		//Metody w interfejsie są domyślnie publiczne i abstarkcyjne
+    String decrypt(String s);		//Pola w interfejsie domyślnie są statyczne i finalneP
+}
